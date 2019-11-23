7135fc949043eefe14c66cb4308b8b9144f3b580
Bonkook Koo <djmimi@nate.com>
2019-11-23 15:56:55 +0900

    Add menu id

---
+++ src/constants/menuId.js
@@ -0,0 +1,11 @@
+const menuId = {
+  ITEM: 'item',
+  BID: 'bid',
+
+  MANAGEMENT: 'management',
+
+  MANAGEMENT_USER: 'managementUser',
+  MANAGEMENT_ETC: 'managementEtc',
+}
+
+export default menuId;
