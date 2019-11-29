4568e4b52e85bd378c00681f8580aaf5a9befb1b
justinaus <justinaus2k@gmail.com>
2019-11-29 14:12:24 +0900

    Add bootstrap-vue, Update line rule

--- src/main.js
+++ src/main.js
@@ -7,12 +7,19 @@ import apiPath from './services/apiPath'
 import jvc from '@justinaus/vue-components'
 import '@justinaus/vue-components/dist/justinaus.css'
 
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
 Vue.prototype.$routerPath = routerPath;
 Vue.prototype.$apiPath = apiPath;
 
 Vue.use(jvc.Plugin)
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
