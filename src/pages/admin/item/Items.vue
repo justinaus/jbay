e002feaa91ffe368d3ac7cbdd7045def5bfb3e5b
Bonkook Koo <djmimi@nate.com>
2019-12-01 04:14:00 +0900

    Update page items for test

--- src/pages/admin/item/Items.vue
+++ src/pages/admin/item/Items.vue
@@ -1,6 +1,38 @@
 <template>
   <PageLayout>
     items
+    <b-button @click="onClick">test</b-button>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
+    <div>akfdajldfjk</div>
   </PageLayout>
 </template>
 
@@ -9,12 +41,25 @@ import PageLayout from '@/components/layout/PageLayout'
 
 import ListPageMixin from '@/mixins/ListPageMixin'
 
+import { SHOW_ALERT_ACTION } from '@/store/modules/alert/action'
+
 export default {
   mixins: [
     ListPageMixin,
   ],
   components: {
     PageLayout,
+  },
+  methods: {
+    onClick() {
+      const params = {
+        text: 'this is test.',
+        onClickPrimary: () => {
+          console.log( 123 );
+        }
+      }
+      this.$store.dispatch( SHOW_ALERT_ACTION, params );
+    }
   }
 }
 </script>
