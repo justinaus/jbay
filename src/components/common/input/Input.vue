43069c5f19454d2927253ed5cdce5a3927c7da9f
justinaus <justinaus2k@gmail.com>
2020-01-21 18:59:28 +0900

    Add common component input

---
+++ src/components/common/input/Input.vue
@@ -0,0 +1,62 @@
+<template>
+  <input
+    v-model="input"
+    :type="type"
+    :placeholder="placeholder"
+    :maxLength="maxLength"
+    :disabled="disabled"
+    @focus="onFocus"
+    @blur="onBlur"
+    @input="onInput"
+    ref="input"
+    v-on:keyup.13="onKeyUpEnter"
+  />
+</template>
+
+<script>
+// input에 대한 공통 스타일 여기에 정의.
+
+export default {
+  props: {
+    value: String,
+    type: {
+      type: String,
+      default: 'text',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: String,
+    maxLength: Number,
+  },
+  computed: {
+    input: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('update:value', value);
+      },
+    },
+  },
+  methods: {
+    blur() {
+      // 외부에서 호출 될 메서드.
+      this.$refs.input.blur();
+    },
+    onFocus(e) {
+      this.$emit('onFocus', e);
+    },
+    onBlur(e) {
+      this.$emit('onBlur', e);
+    },
+    onEnterKeyUp(e) {
+      this.$emit('onEnterKeyUp', e);
+    },
+    onInput(e) {
+      this.$emit('onInput', e);
+    },
+  },
+};
+</script>
