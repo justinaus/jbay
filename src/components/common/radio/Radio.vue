0a0d5de0fb2b091b40ae472d4c5e41991c9a0c8c
justinaus <justinaus2k@gmail.com>
2020-01-21 18:59:49 +0900

    Add common component radio

---
+++ src/components/common/radio/Radio.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <RadioItem
+      v-for="(item, index) in dataList"
+      :key="index"
+      :name="name"
+      :id="item.id"
+      :text="item.text"
+      :disabled="disabled || item.disabled"
+      :selectedId.sync="selectedIdSync"
+      @onChange="onChange"
+    />
+  </div>
+</template>
+
+<script>
+import RadioItem from './RadioItem';
+
+export default {
+  name: 'Radio',
+  components: {
+    RadioItem,
+  },
+  props: {
+    dataList: Array,
+
+    name: String,
+    selectedId: undefined,
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    selectedIdSync: {
+      get() {
+        return this.selectedId;
+      },
+      set(value) {
+        this.$emit('update:selectedId', value);
+      },
+    },
+  },
+  methods: {
+    onChange(to, from) {
+      this.$emit('onChange', to, from);
+    },
+  },
+};
+</script>
---
+++ src/components/common/radio/RadioItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input
+      type="radio"
+      :name="name"
+      :id="id"
+      :value="id"
+      :disabled="disabled"
+      v-model="input"
+      @change="onChange"
+    />
+    <label :for="id">{{ text }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RadioItem',
+  props: {
+    name: String, // radio name
+    id: undefined,
+    selectedId: undefined,
+    text: String,
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    input: {
+      get() {
+        return this.selectedId;
+      },
+      set(value) {
+        this.$emit('update:selectedId', value);
+      },
+    },
+  },
+  methods: {
+    onChange(e) {
+      const from = this.selectedId;
+      const to = e.target.value;
+
+      console.log(from, to);
+
+      this.$emit('onChange', to, from);
+    },
+  },
+};
+</script>
