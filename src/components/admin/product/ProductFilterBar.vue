bbca899b226e835b109010ea007470eccd5a35fb
justinaus <justinaus2k@gmail.com>
2020-01-22 18:58:56 +0900

    Add product filterbar

---
+++ src/components/admin/product/ProductFilterBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Radio
+      :name="radioData.name"
+      :dataList="radioData.dataList"
+      :selectedId.sync="radioData.selectedId"
+    />
+    <Input :value.sync="inputValue" @onEnterKeyUp="onSearch" />
+    <button @click="onSearch">Search</button>
+  </div>
+</template>
+
+<script>
+import Input from '@/components/common/input/Input';
+import Radio from '@/components/common/radio/Radio';
+
+export default {
+  components: {
+    Input,
+    Radio,
+  },
+  data() {
+    return {
+      inputValue: '',
+      radioData: {
+        name: 'radio0',
+        dataList: [
+          { id: 'id', text: 'id' },
+          { id: 'userId', text: 'userId' },
+          { id: 'title', text: 'title' },
+          { id: 'completed', text: 'completed' },
+        ],
+        selectedId: 'title',
+      },
+    };
+  },
+  methods: {
+    onSearch() {
+      console.log('search', this.radioData.selectedId, this.inputValue);
+    },
+  },
+};
+</script>
