f9fd1472577097eea59586cb6b0a228299d148a9
justinaus <justinaus2k@gmail.com>
2020-01-31 10:46:13 +0900

    Update annotation bootstrap

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ import apiPath from './services/apiPath';
 import jvc from '@justinaus/vue-components';
 import '@justinaus/vue-components/dist/justinaus.css';
 
-import BootstrapVue from 'bootstrap-vue';
+// import BootstrapVue from 'bootstrap-vue';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import VScrollLock from 'v-scroll-lock';
 
@@ -21,7 +21,7 @@ Vue.prototype.$routerPath = routerPath;
 Vue.prototype.$apiPath = apiPath;
 
 Vue.use(jvc.Plugin);
-Vue.use(BootstrapVue);
+// Vue.use(BootstrapVue);
 Vue.use(VScrollLock);
 
 new Vue({
