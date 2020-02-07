57011ed682b2482a48418f24f19a6135462d2b2a
justinaus <justinaus2k@gmail.com>
2020-02-07 14:12:50 +0900

    Update public path

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   css: {
     sourceMap: true,
   },
-  publicPath: '', // 이거 없이 컴파일해서 올렸을 때 404 떨어짐.
+  publicPath: '/vue-admin/', // 이거 없이 컴파일해서 올렸을 때 404 떨어짐.
   //   devServer: {
   //     proxy: {
   //       '/api': {
