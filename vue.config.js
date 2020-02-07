6dc713ff2c9e450d6b639f934924d51951b933e4
justinaus <justinaus2k@gmail.com>
2020-02-07 19:06:24 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
     sourceMap: true,
   },
   // 이거 없이 컴파일해서 올렸을 때 404 떨어짐.
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-admin/' : '/',
+  publicPath: '',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vue-admin/' : '/',
   outputDir: 'docs',
   //   devServer: {
   //     proxy: {
